545e65440e4891111decaf16acb901e6d5616a55
Onyeka Igabari <igabs02@yahoo.com>
2014-08-20 23:05:48 -0400

    added datepicker and cleaned up button listeners implementation

--- application/static/js/actions.js
+++ application/static/js/actions.js
@@ -19,37 +19,55 @@ function set_data(tag, data) {
     console.log(" saved data: " + JSON.stringify($(tag).data("job")) );
 }
 
-function get_data(url, job) {
-    var obj = {
-        jobkey: "{{job.jobkey}}",
-        jobtitle: "{{job.jobtitle}}",
-        company: "{{job.company}}",
-        city: "{{job.city}}",
-        state: "{{job.state}}",
-        snippet: "{{job.snippet}}"
-    }
-    /*console.log(" url is: " + url + " data: " + JSON.stringify(job));
-    $.ajax({
-        url: "" + url,
-        type: "GET",
-        data: obj,
-        success: function(data, status, xhr) {
-            //console.log(data);
-        },
-        error: function(xhr, status, error) {
-            console.log(xhr, status, error);
-        }
-    });*/
+function set_form(doc,obj) {
+    var c_div = $(obj).closest("div");
+    var parent = $(c_div).parent();
+    var form_elem = $(parent).children("input")
+    $(form_elem).val(doc);
+}
+
+function split_id(id) {
+
 }
 $(document).ready(function() {
-    $("button").click(function(event) {
-        //var request_path = event.target.id;
+    $(".datepicker").datepicker('update').on('changeDate', function(ev) {
+        console.log(" got a change");
+        $(this).datepicker('hide');
+    });
+
+    $(".delete_button").click(function(event) {
         var id = $(this).attr("id");
         id = id.split("_");
-        console.log(" button: " + id[0] + " nickname: " + id[1] + " job: " + id[2]);
-        if(id[0] == "save") {
+        if (id != undefined) {
+            console.log(" delete job: " + id[2]);
             $.ajax({
-                url: "/user/" + id[1] + "/" + id[2],
+                url: "/user/" + id[1] + "/bookmarked/" + id[2],
+                type: "GET",
+                success: function(data, status, xhr) {
+                    //console.log(data);
+                    $(document.body).html(data);
+                },
+                error: function(xhr, status, error) {
+                    console.log(xhr, status, error);
+                }
+            });
+        }
+    });
+
+    $(".apply_button").click(function(event) {
+        var id = $(this).attr("id");
+        id = id.split("_");
+        if (id != undefined) {
+            console.log(" apply for job: " + id[2]);
+            /*alert(" Would you like to apply for job id: " + id[2] + " ?");
+            var r = confirm("Press a button");
+            if (r == true) {
+                x = "You pressed OK!";
+            } else {
+                x = "You pressed Cancel!";
+            }*/
+            $.ajax({
+                url: "/user/" + id[1] + "/apply/" + id[2],
                 type: "GET",
                 success: function(data, status, xhr) {
                     //console.log(data);
@@ -59,10 +77,14 @@ $(document).ready(function() {
                 }
             });
         }
-        else if(id[0] == "delete") {
-            console.log(" delete job: " + id[2]);
+    });
+
+    $(".save_button").click(function(event) {
+        var id = $(this).attr("id");
+        id = id.split("_");
+        if (id != undefined) {
             $.ajax({
-                url: "/user/" + id[1] + "/bookmarked/" + id[2],
+                url: "/user/" + id[1] + "/" + id[2],
                 type: "GET",
                 success: function(data, status, xhr) {
                     //console.log(data);
